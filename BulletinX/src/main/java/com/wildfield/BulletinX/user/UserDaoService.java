a13afcc7cb6b0ff05650b7659e4fc2f8228f6953
Sweetsweet <tianye35010@gmail.com>
2025-07-25 00:48:31 -0400

    03-create-user

--- BulletinX/src/main/java/com/wildfield/BulletinX/user/UserDaoService.java
+++ BulletinX/src/main/java/com/wildfield/BulletinX/user/UserDaoService.java
@@ -9,16 +9,29 @@ import java.util.List;
 @Component
 public class UserDaoService {
     private static List<User> users = new ArrayList<>();
+
+    //全类共享static
+    private static int usersCount = 0;
+
     static{
-        users.add(new User(1,"doll", LocalDate.now().minusYears(30)));
-        users.add(new User(2,"toy", LocalDate.now().minusYears(25)));
-        users.add(new User(3,"visa", LocalDate.now().minusYears(20)));
+        users.add(new User(++usersCount,"doll", LocalDate.now().minusYears(30)));
+        users.add(new User(++usersCount,"toy", LocalDate.now().minusYears(25)));
+        users.add(new User(++usersCount,"visa", LocalDate.now().minusYears(20)));
     }
+
+    public User save(User user){
+        user.setId(++usersCount);
+        users.add(user);
+        return user;
+    }
+
+
     public List<User> findAll(){
         return users;
     }
 
     public User findOne(int id){
+        //根据id筛选，返回第一个
         return users.stream()
                 .filter(user -> user.getId().equals(id))
                 .findFirst()
